c2fb5a28851b14baa225c9ce89e60b31e908525c
begioqropiridze <begiokro@gmail.com>
2025-07-21 20:49:58 +0400

    commit

---
+++ level03/classwork/classwork.py
@@ -0,0 +1,24 @@
+number=10
+number2= 15.3
+print(number)
+print(number2)
+print(number+ number2)
+
+
+12+13
+36+37
+
+13-9
+99-37
+
+12*9
+80*15
+
+12/6
+70/10
+
+name="begi"
+lastname="OQROPIRIDZE"
+print(name)
+print(lastname)
+print(name+lastname)
---
+++ level03/homework/homework.py
@@ -0,0 +1,49 @@
+number1=10
+number2= 15
+number3=20
+print(number1)
+print(number2)
+print(number3)
+print(number1+ number2+ number3)
+
+
+x=8
+y=9
+print(x)
+print(y)
+print(x+y)
+
+
+length=20
+width=10
+print(length)
+print(width)
+print(length+width+length+width)
+
+# არის + მიმატება
+print(5+7)
+print(234+65)
+print(12+2)
+print(7+9)
+print(3+5)
+
+# არის - გამოკლება
+print(17-4)
+print(3-1)
+print(7-5)
+print(5-3)
+print(1585-952)
+
+# არის * გამრავლება
+print(2*4)
+print(12*4)
+print(35*34)
+print(779*3)
+print(53*73)
+
+# არის / გაყოფა
+print(2.5//3)
+print(12/6)
+print(34/7)
+print(6432/8)
+print(15/5)
